ef0be8ad5832fe2f8f8ca62b747139d4beb8a9a4
yoonxjoong <yhj8372@naver.com>
2024-09-22 23:53:42 +0900

    init

---
+++ ch8_event/src/main/java/com/example/ch8_event/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.ch8_event
+
+import android.os.Bundle
+import android.os.SystemClock
+import android.view.KeyEvent
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.ch8_event.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var initTime = 0L
+    private var pauseTime = 0L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startButton.setOnClickListener {
+            binding.chronometer.base = SystemClock.elapsedRealtime() + pauseTime
+            binding.chronometer.start()
+
+            binding.stopButton.isEnabled = true
+            binding.resetButton.isEnabled = true
+            binding.startButton.isEnabled = false
+        }
+
+        binding.stopButton.setOnClickListener {
+            pauseTime = binding.chronometer.base - SystemClock.elapsedRealtime()
+            binding.chronometer.stop()
+
+            binding.stopButton.isEnabled = false
+            binding.resetButton.isEnabled = true
+            binding.startButton.isEnabled = true
+        }
+
+        binding.resetButton.setOnClickListener {
+            pauseTime = 0L
+            binding.chronometer.base = SystemClock.elapsedRealtime()
+            binding.chronometer.stop()
+
+            binding.stopButton.isEnabled = false
+            binding.resetButton.isEnabled = false
+            binding.startButton.isEnabled = true
+        }
+
+    }
+}
